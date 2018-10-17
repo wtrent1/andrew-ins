074ba7e643a18f717776857f375d7e808d9cb1f0
Walker Trent <walktrent@gmail.com>
2018-10-17 01:01:05 +0000

    basic layout mostly functioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  get 'about', to: 'pages#about'
+  get 'bio', to: 'pages#bio'
+  get 'projects', to: 'pages#projects'
+  get 'academic', to: 'pages#academic'
+  resources :contacts, only: :create
+  get 'contact_me', to: 'contacts#new', as: 'contact_me'
 end
