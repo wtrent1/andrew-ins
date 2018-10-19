5aeb30e23a25a398b8316faf13aefc6fbc60c1e5
Walker Trent <walktrent@gmail.com>
2018-10-19 01:43:32 +0000

    Fixed contact databaseand routing issue

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,20 @@
 class ContactsController < ApplicationController
-    def new
+  def new
+    @contact = Contact.new
+  end
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to contact_me_path, notice: "Message sent."
+    else
+      redirect_to contact_me_path, notice: "Error occured."
+    end
+  end
+  
+  # To collect data from form, strong parameters were used and form fields were
+  # whitelisted
+  private
+    def contact_params
+      params.require(:contact).permit(:name, :email, :comments)
     end
 end
